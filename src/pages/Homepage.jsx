53fe3a67e1a12b32c7ba7f0035490e8745f8b7a5
Greg <gajdukow@protonmail.com>
2024-05-13 18:49:34 +0200

    adjust homepage layout

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -32,9 +32,9 @@ function Homepage() {
         {articles.length > 0 && 
             articles.map(article =>{
                 return (
-                    <div className="grid grid-cols-1 md:grid-cols-4 w-4/5 gap 4 p-2 border-2 m-auto mt-5" key={article.id}>
+                    <div className="grid grid-cols-1 md:grid-cols-5 w-4/5 gap 4 p-2 border-2 m-auto mt-5" key={article.id}>
                         <div className="col-span-2"><img src="https://media.formula1.com/image/upload/f_auto,c_limit,w_1242,q_auto/t_16by9Centre/f_auto/q_auto/fom-website/2024/Imola%20(Emilia-Romagna)/It's_Race_Week_Imola_V1"/></div>
-                        <div className="ml-2 col-span-2">
+                        <div className="ml-2 col-span-3">
                             <h1 className="text-lg font-bold">{article.title}</h1>
                             <p className="text-lg" >{article.date} - {article.subtitle}</p>
                         </div>
