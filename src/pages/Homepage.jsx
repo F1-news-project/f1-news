3a14f55f9a7ba391a50d85131bb9ba10dae8596c
Greg <gajdukow@protonmail.com>
2024-05-13 18:43:33 +0200

    adjust homepage breakpoints

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -24,7 +24,7 @@ function Homepage() {
             <div><img src="https://media.formula1.com/image/upload/f_auto,c_limit,w_1242,q_auto/t_16by9Centre/f_auto/q_auto/fom-website/2024/Imola%20(Emilia-Romagna)/It's_Race_Week_Imola_V1"/></div>
             <div className="ml-2">
                 <h1 className="text-3xl font-bold">{articles.length > 0 && articles[0].title}</h1>
-                <p className="text-lg" >{articles.length > 0 && articles[0].subtitle}</p>
+                <p className="text-lg" >Date - {articles.length > 0 && articles[0].subtitle}</p>
             </div>
         </div>
  
@@ -32,14 +32,11 @@ function Homepage() {
         {articles.length > 0 && 
             articles.map(article =>{
                 return (
-                    <div className="grid grid-cols-4 w-4/5 gap 4 p-2 border-2 m-auto" key={article.id}>
-                        <div><img src="https://media.formula1.com/image/upload/f_auto,c_limit,w_1242,q_auto/t_16by9Centre/f_auto/q_auto/fom-website/2024/Imola%20(Emilia-Romagna)/It's_Race_Week_Imola_V1"/></div>
-                        <div className="ml-2">
-                            <h1 className="text-lg font-bold">{article.title}</h1>{article.date}
-                        </div>
-                        <div><img src="https://media.formula1.com/image/upload/f_auto,c_limit,w_1242,q_auto/t_16by9Centre/f_auto/q_auto/fom-website/2024/Imola%20(Emilia-Romagna)/It's_Race_Week_Imola_V1"/></div>
-                        <div className="ml-2">
-                            <h1 className="text-lg font-bold">{article.title}</h1>{article.date}
+                    <div className="grid grid-cols-1 md:grid-cols-4 w-4/5 gap 4 p-2 border-2 m-auto mt-5" key={article.id}>
+                        <div className="col-span-2"><img src="https://media.formula1.com/image/upload/f_auto,c_limit,w_1242,q_auto/t_16by9Centre/f_auto/q_auto/fom-website/2024/Imola%20(Emilia-Romagna)/It's_Race_Week_Imola_V1"/></div>
+                        <div className="ml-2 col-span-2">
+                            <h1 className="text-lg font-bold">{article.title}</h1>
+                            <p className="text-lg" >{article.date} - {article.subtitle}</p>
                         </div>
                     </div>
                 )
