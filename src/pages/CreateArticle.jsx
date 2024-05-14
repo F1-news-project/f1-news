6db0372b4373b176132d608025fff294aed2a5f4
Greg <gajdukow@protonmail.com>
2024-05-14 16:37:45 +0200

    add featured article checkbox

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -6,6 +6,7 @@ function CreateArticle() {
   const [title, setTitle] = useState("");
   const [subtitle, setSubtitle] = useState("");
   const [text, setText] = useState("");
+  const [isFeatured, setIsFeatured] = useState(false)
 
   function getDate() {
     const today = new Date();
@@ -24,7 +25,7 @@ function CreateArticle() {
       subtitle,
       text,
       date: getDate(),
-      featured: false
+      featured: isFeatured
     };
 
     axios
@@ -72,7 +73,7 @@ function CreateArticle() {
                   />
                 </label>
               </div>
-              <div className="mb-8">
+              <div>
                 <label className="text-xl text-gray-600">
                   Article Description:
                   <textarea
@@ -87,6 +88,26 @@ function CreateArticle() {
                   />
                 </label>
               </div>
+              <div className="inline-flex items-center">
+                <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="check">
+                  <input type="checkbox"
+                    className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10"
+                    id="check" checked={isFeatured} onChange={(e) => setIsFeatured(!isFeatured)}/>
+                  <span
+                    className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                      stroke="currentColor" strokeWidth="1">
+                      <path fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"></path>
+                    </svg>
+                  </span>
+                </label>
+                <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="check">
+                  Featured Article
+                </label>
+              </div>
+
               <div className="flex p-1">
                 <button className="p-3 bg-blue-500 text-white hover:bg-blue-400">
                   Create new article
