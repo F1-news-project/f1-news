7cc2bcfcfd457b636e2753c9c80858afb0f9b742
Greg <gajdukow@protonmail.com>
2024-05-15 10:10:28 +0200

    add formatting to code

--- src/pages/Drivers.jsx
+++ src/pages/Drivers.jsx
@@ -15,14 +15,13 @@ function Drivers() {
       });
   }, []);
   return (
-    <div className=" grid grid-cols-2 gap-4 place-content-around h-48 mt-3  ">
+    <div className="grid grid-cols-2 gap-4 place-content-around h-48 mt-3">
       {driver.length > 0 &&
         driver.map((driver) => {
           return (
             <div
-              className="bg-blue-100 rounded-lg text-center max-w-80  bg-transparent border-2"
-              key={driver.id}
-            >
+              className="bg-blue-100 rounded-lg text-center max-w-80 bg-transparent border-2"
+              key={driver.id}>
               {<div className="font-bold">{driver.name}</div>}{" "}
               {<h1>World Titles: {driver.worldTitles}</h1>}
               {
