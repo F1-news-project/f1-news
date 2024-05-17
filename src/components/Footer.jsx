19d294da08f485673819569ad00c405d97f2ad4f
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-17 10:59:57 +0200

    add target _blank to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div>
       <footer className="footer flex  items-center text-center no-underline decoration-black text-md p-4 mt-4 ">
-        <Link to="https://github.com/F1-news-project/f1-news" className="">
+        <Link to="https://github.com/F1-news-project/f1-news" target="_blank">
           GITHUB REPOSITORY
         </Link>
       </footer>
